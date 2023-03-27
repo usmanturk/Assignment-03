84fe0bc9e4fe4c66e73946e84ab8124399e91a90
usmanturk <126104161+usmanturk@users.noreply.github.com>
2023-03-28 00:50:55 +0500

    Add files via upload

---
+++ assignment 3/Q1Q3andQ5.js
@@ -0,0 +1,25 @@
+console.log("Assignment 03");
+console.log("_____________");
+console.log("Data Type conversion");
+console.log("___________________");
+const age = 25;
+console.log(age);
+console.log(typeof age);
+const ageasstring = String(age);
+console.log(ageasstring, typeof ageasstring);
+console.log("___________________");
+console.log("Unit conversion");
+console.log("___________________");
+const temp = 72;
+const celcius = ((temp - 32) * 5) / 9;
+console.log("Temperature in Farhenheight " + temp + "F");
+console.log("Temperature in Celcius " + celcius + "C");
+console.log("______________________");
+console.log("Rain Precautions");
+console.log("________________");
+const Rain = true;
+if (Rain == true) {
+  console.log("Bring an Umbrella");
+} else {
+  console.log("Leave Umbrella at home");
+}
